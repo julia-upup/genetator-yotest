8abca91c097c862e6cc6c65d963bf4f64214d667
lijiazi <lijiazi@jd.com>
2019-11-14 15:23:43 +0800

    second commit

---
+++ generators/app/index.js
@@ -0,0 +1,36 @@
+const Generator = require('yeoman-generator');
+
+module.exports = class extends Generator {
+  // The name `constructor` is important here
+  constructor(args, opts) {
+    // Calling the super constructor is important so our generator is correctly set up
+    super(args, opts);
+
+    // Next, add your custom code
+    this.option('babel'); // This method adds support for a `--babel` flag
+  }
+
+  // add your own methods
+  prompting() {
+    const prompts = [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Project name:',
+        default: 'test-project'
+      },
+      {
+        type: 'list',
+        name: 'framework',
+        message: 'choose a framework',
+        choices: ['React', 'Vue', 'Angular'],
+        default: 'React'
+      }
+    ];
+
+    this.prompt(prompts).then(answers => {
+      this.name = answers.name;
+      this.framework = answers.framework;
+    })
+  }
+};
